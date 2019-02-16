00cee5a40ecfb3e691bb05f8a4e2a3ad03eaec51
Neuie <gneuendorf@gmail.com>
2019-02-16 12:05:20 -0500

    Update handlebars and routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,10 +3,10 @@ var db = require("../models");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
+    db.Example.findAll({}).then(function(dbEntries) {
       res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
+        msg: "iniGEN - Change Your World",
+        examples: dbEntries
       });
     });
   });
