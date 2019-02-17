5c59e302590f7998f74cdb08f32a8158363bac43
shawnathanmcfly <sachimasi9187@gmail.com>
2019-02-17 10:53:25 -0500

    merge me jerk

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,18 +2,18 @@ var db = require("../models");
 
 module.exports = function(app) {
   // Load index page
+
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
+    db.IniFile.findAll({}).then(function(dbExamples) {
       res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
+        file: dbExamples
       });
     });
   });
 
   // Load example page and pass in an example by id
   app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+    db.IniFile.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
       res.render("example", {
         example: dbExample
       });
