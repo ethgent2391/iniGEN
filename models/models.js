7b286a8c0eb734aea2de2b1f143f0c92ecb880dd
shawnathanmcfly <sachimasi9187@gmail.com>
2019-02-20 13:11:17 -0500

    fetches data by id

--- models/models.js
+++ models/models.js
@@ -23,27 +23,46 @@ module.exports = (sequelize, DataTypes) => {
 
     function IniFile( q, cb) {
 
+        let stop, en = 0;
+        //stop iterator if a number is in string
+        for( stop = 0; stop < q.length; stop++ ){    
+            if( !isNaN( parseInt( q[stop] ) ) ){
+
+                en = parseInt( q.substring( stop ) );
+                q = q.substring( 0, stop );
+                break;
+            }
+        }
+
         switch( q ){
-            case "GETNAMES":
+            case "GET_ALL":
             sequelize.query("SELECT * FROM IniFiles").then( dat => {
+
                 let data = [];
-    
+
                 dat = dat[0];
-    
+
                 for( let i = 0; i < dat.length; i++ ){
                     let t = JSON.parse(dat[i]['/Script/EngineSettings']);
                     data.push( { id: dat[i].id, name: t.GeneralProjectSettings.ProjectName });
                 }
                     
-                cb(data);         
+                cb(data); 
+            });
+            break;
+            case "GET_BY_ID":
+            sequelize.query("SELECT * FROM IniFiles WHERE id = " + en ).then( dat => {
+    
+                dat = dat[0][0];            
+                    
+                cb(dat);
             });
             break;
             default:
+                console.log( q + " is an invalid command.");
             break;
         }
-        
-
     }
-        
+
    return IniFile ;
 }
