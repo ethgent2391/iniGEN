f12dc6dcaf4894c6b19ba307413dbd76bcc0d58c
shawnathanmcfly <sachimasi9187@gmail.com>
2019-02-21 21:32:27 -0500

    ajax is the best ok

--- public/js/index.js
+++ public/js/index.js
@@ -6,58 +6,24 @@ var $exampleList = $("#example-list");
 
 // The API object contains methods for each kind of request we'll make
 var API = {
-  saveExample: function(example) {
+  getExamples: function(id) {
     return $.ajax({
-      headers: {
-        "Content-Type": "application/json"
-      },
-      type: "POST",
-      url: "api/examples",
-      data: JSON.stringify(example)
-    });
-  },
-  getExamples: function() {
-    return $.ajax({
-      url: "api/examples",
-      type: "GET"
-    });
-  },
-  deleteExample: function(id) {
-    return $.ajax({
-      url: "api/examples/" + id,
-      type: "DELETE"
+      url: "/settings/" + id,
+      type: "GET",
+      success: () => {
+        location.reload();
+      }
+      
     });
   }
 };
 
-// refreshExamples gets new examples from the db and repopulates the list
-var refreshExamples = function() {
-  API.getExamples().then(function(data) {
-    var $examples = data.map(function(example) {
-      var $a = $("<a>")
-        .text(example.text)
-        .attr("href", "/example/" + example.id);
-
-      var $li = $("<li>")
-        .attr({
-          class: "list-group-item",
-          "data-id": example.id
-        })
-        .append($a);
-
-      var $button = $("<button>")
-        .addClass("btn btn-danger float-right delete")
-        .text("ｘ");
-
-      $li.append($button);
-
-      return $li;
-    });
-
-    $exampleList.empty();
-    $exampleList.append($examples);
-  });
-};
+$(
+  $("#main-list").on("click", ".ini-select", function(){
+     
+     API.getExamples($(this).attr("data-id"));
+  })
+);
 
 // handleFormSubmit is called whenever we submit a new example
 // Save the new example to the db and refresh the list
